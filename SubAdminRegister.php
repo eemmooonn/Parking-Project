19d7bf874fbf34beb07db385f7a33914f34148b5
eemmooonn <mazumderemon@gmail.com>
2022-05-19 03:06:57 +0600

    1

--- SubAdminRegister.php
+++ SubAdminRegister.php
@@ -1,7 +1,3 @@
-<?php 
-  require("connect.php");
-?>
-
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -27,34 +23,59 @@
       </div>
       <div class="forms">
         <div class="form-content">
-          <div class="login-form">
-            <div class="title">Login</div>
-            <form method="POST">
+          <div class="signup-form">
+            <div class="title">Register</div>
+            <form action="insert_subAdmin.php" method="POST">
               <div class="input-boxes">
                 <div class="input-box">
+                  <i class="fas fa-user"></i>
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder="Enter your name"
+                    required
+                  />
+                </div>
+
+                <div class="input-box">
+                  <i class="fas fa-phone"></i>
+                  <input
+                    type="number"
+                    name="phone"
+                    placeholder="Enter your phone number"
+                    required
+                  />
+                </div>
+                <div class="input-box">
                   <i class="fas fa-envelope"></i>
-                  <input type="text" name="email" placeholder="Enter your email" required />
+                  <input
+                    type="text"
+                    name="email"
+                    placeholder="Enter your email"
+                    required
+                  />
                 </div>
                 <div class="input-box">
                   <i class="fas fa-lock"></i>
                   <input
-                    type="password" name="password"
+                    type="password"
+                    name="password"
                     placeholder="Enter your password"
                     required
                   />
                 </div>
-                <div class="text"><a href="#">Forgot password?</a></div>
                 <div class="button input-box">
                   <input
                     type="submit"
-                    value="Login"
-                    name="login"
-                    class="login"
+                    name="submit"
+                    class="submit"
+                    value="Sumbit"
                   />
                 </div>
+
                 <div class="text sign-up-text">
-                  Don't have an account?
-                  <a href="SubAdminRegister.php">Register now</a>
+                  Already have an account?
+                  <a href="SubAdminLogin.php">Login now</a>
                 </div>
               </div>
             </form>
@@ -64,22 +85,3 @@
     </div>
   </body>
 </html>
-
-<?php
-
-if(isset($_POST['login']))
-{
-  $query="SELECT * FROM `subadmin_list` WHERE email='$_POST[email]' AND password='$_POST[password]'";
-  $result=mysqli_query($con,$query);
-  if (mysqli_num_rows($result)==1)
-  {
-    session_start();
-    $_SESSION['AdminLoginEmail']=$_POST['email'];
-    header("location:SubAdmin.php");
-  }
-  else {
-    echo "<script>alert('Incorrect Email or Password!') </script>";
-  }
-}
-
-?>
---
+++ insert_subAdmin.php
@@ -0,0 +1,50 @@
+<?php
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+if(!empty($name) || !empty($phone) || !empty($email) || !empty($password)){
+    $host = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbName = "parko";
+
+    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+
+    if (mysqli_connect_error()) {
+        die('Connection Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+    } else{
+        $SELECT = "SELECT email From subadmin_list Where email = ? Limit 1";
+        $INSERT = "INSERT Into subadmin_list (name,phone,email,password) values(?,?,?,?)";
+
+        //prepare statement
+        $stmt = $conn->prepare($SELECT);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $stmt->bind_result($email);
+        $stmt->store_result();
+        $rnum = $stmt->num_rows;
+
+        if($rnum==0) {
+            $stmt->close();
+
+            $stmt = $conn->prepare($INSERT);
+            $stmt->bind_param("siss", $name, $phone, $email, $password);
+            $stmt->execute();
+            echo "Submitted Successfully!";
+        } else{
+            echo "Already have an account using this email!";
+        }
+        $stmt->close();
+        $conn->close();
+    }
+
+
+
+} else {
+    echo "All field are required";
+    die();
+}
+
+?>
